1b81301952952374e9bb352e4cffcf49c3643ba8
Virgilio Nayal <virgilionayal@gmail.com>
2020-09-08 11:11:36 +0200

    change file name 'build' to docs'

---
+++ docs/precache-manifest.60935c160d7cb116ea7bc7f0679bd386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cf70a7e37cb950541bcc79b2daca14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa77f364c56b431f6aa",
+    "url": "/static/css/main.03944f53.chunk.css"
+  },
+  {
+    "revision": "9609f607b7f67dafefa3",
+    "url": "/static/js/2.86c83d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86c83d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa77f364c56b431f6aa",
+    "url": "/static/js/main.c1408da0.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
